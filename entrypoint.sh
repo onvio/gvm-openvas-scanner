6d3130664145f665cb05801f5b1c953df6a0738d
root <d.snel@onvio.nl>
2020-11-10 11:03:21 +0100

    Wait until all NVT's are synced.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,16 @@ fi
 
 /start.sh
 
+config_exists=""
+while [ -z "$config_exists" ]; do
+    echo "Waiting for scan configs to be imported. If this is the first run, this may take 10 minutes."
+    configs=$(su -c "gvm-cli --gmp-username=admin --gmp-password=admin tls --xml '<get_configs />'" gvm)
+    config_exists=$(echo "$configs" | grep "d21f6c81-2b88-4ac1-b7b4-a2a9f2ad4663")
+	sleep 10
+done
+
+echo "Scan configs are imported, running scanscript"
+
 chown gvm:gvm -R /var/reports/
 
 args="$@"
